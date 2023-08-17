de7e1ebf0ee911f9eefa8a4425e175dccdbbdf45
rodercode <rodersnow@gmail.com>
2023-08-17 13:21:12 +0200

    implement simple style header mobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <template>
-  <ion-icon :icon="menu" />
+  <div class="home">
+    <header class="main-header">
+      <div class="brand-logo">Roder.me</div>
+      <nav class="main-nav">
+        <ul class="nav-list">
+          <li class="nav-link">About</li>
+          <li class="nav-link">Project</li>
+          <li class="nav-link">Contact</li>
+        </ul>
+      </nav>
+      <div class="btn btn--menu">
+        <ion-icon class="icon" :icon="menu" />
+      </div>
+    </header>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,10 +22,31 @@ import { menu } from "ionicons/icons";
 
 export default defineComponent({
   name: "HomeView",
-  data(){
-    return{
+  data() {
+    return {
       menu,
-    }
-  }
+    };
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+
+.home{
+  padding: 3.2rem 1.6rem;
+}
+.main-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-list {
+  display: none;
+}
+
+.icon {
+  height: 3.2rem;
+  width: 3.2rem;
+}
+</style>
